44c87030f93eafced8801e21b61dd85f84f7b9c8
srlechuga18 <aledeleon222@hotmail.com>
2020-07-31 14:08:56 -0500

    icon fixed

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/favicon.ico',
+  manifestIcon: 'src/assets/img/favicon-96x96.png',
   pathPrefix: `/gatsby-starter-grayscale/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'HeavenSent',
   subHeading: 'Test ',
