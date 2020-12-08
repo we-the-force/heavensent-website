2c5c7fc95b3e87abce3f7056f174857ac8b35409
Will Alvarez <will.alvarez@gmail.com>
2020-12-08 13:27:34 -0600

    vars

--- src/components/Download.js
+++ src/components/Download.js
@@ -38,10 +38,10 @@ class Download extends Component {
                     </div>
                     <div className="stores">
                         <a href="#download" className="app_store">
-                            <img src={this.getTrans('apple_store.url')} alt=""/>
+                            <img src={this.getTrans('apple_store').url} alt=""/>
                         </a>
                         <a href="#download" className="google_play">
-                            <img src={this.getTrans('google_store.url')} alt=""/>
+                            <img src={this.getTrans('google_store').url} alt=""/>
                         </a>
                     </div>
                 </div>
